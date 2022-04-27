634139f2cac6e550e88464f05454092b2d8f2b6f
AndrewN <andrewnguyen250@gmail.com>
2022-04-27 12:33:08 -0700

    added text to speech

--- app/src/main/java/com/example/team21_zooseeker/MainActivity.java
+++ app/src/main/java/com/example/team21_zooseeker/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.team21_zooseeker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModel;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText search_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        search_bar = findViewById(R.id.search_bar);
+    }
+
+    // Source: https://www.youtube.com/watch?v=0bLwXw5aFOs
+    public void getSpeechInput(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        startActivityForResult(intent, 0);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+        search_bar.setText(result.get(0));
+
     }
 }
