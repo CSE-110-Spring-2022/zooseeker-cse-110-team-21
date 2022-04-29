7d92a8e6ea5759eb02b5d694d73d0f496edea4af
AndrewN <andrewnguyen250@gmail.com>
2022-04-28 21:06:50 -0700

    Added autocomplete (might be broken)

--- app/src/main/java/com/example/team21_zooseeker/MainActivity.java
+++ app/src/main/java/com/example/team21_zooseeker/MainActivity.java
@@ -1,28 +1,47 @@
 package com.example.team21_zooseeker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.lifecycle.ViewModel;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText search_bar;
+    private AutoCompleteTextView search_bar;
+    public  ArrayList<String> animalNames;
+
+    // ignore copied from
+    // https://www.youtube.com/watch?v=JB3ETK5mh3c
+    private static final String[] COUNTRIES = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain"
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         search_bar = findViewById(R.id.search_bar);
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, COUNTRIES);
+        search_bar.setAdapter(adapter);
+
     }
 
+
     // Source: https://www.youtube.com/watch?v=0bLwXw5aFOs
+    // gets speech from user and places the words into search_Bar
     public void getSpeechInput(View view) {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
@@ -36,9 +55,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         super.onActivityResult(requestCode, resultCode, data);
-        if (data!= null) {
+        if (data != null && resultCode == 0) {
             ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            search_bar.setText(result.get(0));
+            search_bar.setText(result.get(0), true);
 
         }
 
