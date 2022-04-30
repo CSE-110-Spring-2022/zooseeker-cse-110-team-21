5dc5888e4e8f9b21e0e885a3867571a71c34db5e
Nyan Min Htet <niallhtet@gmail.com>
2022-04-30 04:13:48 -0700

    Niall Progress so far - clicking button now adds to list of animals selected

--- app/src/main/java/com/example/team21_zooseeker/MainActivity.java
+++ app/src/main/java/com/example/team21_zooseeker/MainActivity.java
@@ -1,25 +1,47 @@
 package com.example.team21_zooseeker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuView;
 import androidx.lifecycle.ViewModel;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private EditText search_bar;
+    private List<String> selectedAnimals = new ArrayList<>(Arrays.asList());
+    private Button selectButton;
+    private Button selectButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         search_bar = findViewById(R.id.search_bar);
+
+        Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON(this,"sample_node_info.json");
+
+        this.selectButton = (Button)findViewById(R.id.button);
+        String firstVertex = vInfo.get(vInfo.keySet().toArray()[0]).getName();
+        this.selectButton.setText(firstVertex);
+
+        this.selectButton2 = (Button)findViewById(R.id.button2);
+        String secondVertex = vInfo.get(vInfo.keySet().toArray()[1]).getName();
+        this.selectButton2.setText(secondVertex);
+
+        Log.d("vInfo", vInfo.toString());
     }
 
     // Source: https://www.youtube.com/watch?v=0bLwXw5aFOs
@@ -35,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (data!= null) {
+        if (data != null) {
             ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
             search_bar.setText(result.get(0));
 
@@ -43,7 +65,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onAnimalButtonClick(View view) {
-
+        // Push our animal object_id into List<object_id>
+        this.selectButton = (Button)findViewById(view.getId());
+        this.selectedAnimals.add(selectButton.getText().toString());
+        for (String animal : selectedAnimals) {
+            Log.d("animal name", animal);
+        }
     }
-
 }
--- app/src/main/java/com/example/team21_zooseeker/ZooData.java
+++ app/src/main/java/com/example/team21_zooseeker/ZooData.java
@@ -1,13 +1,17 @@
 package com.example.team21_zooseeker;
 
+import android.content.Context;
+
 import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.lang.reflect.Type;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -26,32 +30,47 @@ public class ZooData {
         public Kind kind;
         public String name;
         public List<String> tags;
+
+        @Override
+        public String toString() {
+            return "\nVertexInfo{" +
+                    "id='" + id + '\'' +
+                    ", kind=" + kind +
+                    ", name='" + name + '\'' +
+                    ", tags=" + tags +
+                    '}';
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 
-    public static class EdgeInfo {
-        public String id;
-        public String street;
-    }
+    public static Map<String, VertexInfo> loadVertexInfoJSON(Context context, String path) {
+        try {
+            InputStream inputStream = context.getAssets().open(path);
+            Reader reader = new InputStreamReader(inputStream);
 
-    public static Map<String, VertexInfo> loadVertexInfoJSON(String path) {
-        InputStream inputStream = MainActivity.class.getClassLoader().getResourceAsStream(path);
-        Reader reader = new InputStreamReader(inputStream);
+            Gson gson = new Gson();
+            Type type = new TypeToken<List<VertexInfo>>() {
+            }.getType();
+            List<ZooData.VertexInfo> zooData = gson.fromJson(reader, type);
 
-        Gson gson = new Gson();
-        Type type = new TypeToken<List<VertexInfo>>(){}.getType();
-        List<ZooData.VertexInfo> zooData = gson.fromJson(reader, type);
+            // This code is equivalent to:
+            //
+            // Map<String, ZooData.VertexInfo> indexedZooData = new HashMap();
+            // for (ZooData.VertexInfo datum : zooData) {
+            //   indexedZooData[datum.id] = datum;
+            // }
+            //
+            Map<String, ZooData.VertexInfo> indexedZooData = zooData
+                    .stream()
+                    .collect(Collectors.toMap(v -> v.id, datum -> datum));
 
-        // This code is equivalent to:
-        //
-        // Map<String, ZooData.VertexInfo> indexedZooData = new HashMap();
-        // for (ZooData.VertexInfo datum : zooData) {
-        //   indexedZooData[datum.id] = datum;
-        // }
-        //
-        Map<String, ZooData.VertexInfo> indexedZooData = zooData
-                .stream()
-                .collect(Collectors.toMap(v -> v.id, datum -> datum));
-
-        return indexedZooData;
+            return indexedZooData;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Collections.emptyMap();
+        }
     }
 }
