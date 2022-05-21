972ac541ff3d348410657b52d1abfc9ef015c07e
Wesley Kiang <drace@Wesleys-MacBook-Air.local>
2022-05-20 23:49:09 -0700

    typo

--- app/src/main/java/com/example/team21_zooseeker/helpers/PermissionChecker.java
+++ app/src/main/java/com/example/team21_zooseeker/helpers/PermissionChecker.java
@@ -19,7 +19,7 @@ public class PermissionChecker {
         this.activity = activity;
         requestPermissionLauncher = activity.registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), perms -> {
             perms.forEach((perm, isGranted) -> {
-                Log.i("LAB7", String.format("Permission %s granted: %s", perm, isGranted));
+                Log.i("Permission", String.format("Permission %s granted: %s", perm, isGranted));
             });
         });
     }
