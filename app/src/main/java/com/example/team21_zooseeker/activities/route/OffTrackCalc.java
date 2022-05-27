a12aab32ed298a7ab4658f6bb606304bc963fb3d
Wesley Kiang <drace@Wesleys-MacBook-Air.local>
2022-05-27 11:46:04 -0700

    return closest vertex

--- app/src/main/java/com/example/team21_zooseeker/activities/route/OffTrackCalc.java
+++ app/src/main/java/com/example/team21_zooseeker/activities/route/OffTrackCalc.java
@@ -3,53 +3,31 @@ package com.example.team21_zooseeker.activities.route;
 import android.util.Pair;
 
 import com.example.team21_zooseeker.helpers.ZooData;
-import com.google.android.gms.maps.model.LatLng;
 
-import org.jgrapht.GraphPath;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public class OffTrackCalc {
     public static final double DEG_LAT = 363843.57;
     public static final double DEG_LNG = 307515.50;
 
-
-    public static final LatLng test_one = new LatLng(5, -100);
-    public static final LatLng test_two = new LatLng(8, -30);
-
-    //@TODO
-    public static LatLng compare(LatLng one, LatLng two) {
-        return new LatLng(0,0);
-    }
-
-    public List<GraphPath<String, IdentifiedWeightedEdge>> calculateRoute(double currLat, double currLng, List<String> exhibits,
-                                                                          Map<String, ZooData.VertexInfo> vInfo, RouteCalc rc){
-        List<GraphPath<String, IdentifiedWeightedEdge>> route =
-                new ArrayList<GraphPath<String, IdentifiedWeightedEdge>>();
+    public String calculateRoute(double currLat, double currLng, Map<String, ZooData.VertexInfo> vInfo){
 
         Pair<Double,Double> currLoc = new Pair<>(currLat,currLng);
 
         double minDist = Double.MAX_VALUE;
-        String minExhibit = "";
+        String minVertex = "";
 
-        for (String e : exhibits){
-            Pair<Double, Double> eLoc = new Pair<>(Double.parseDouble(vInfo.get(e).lat),Double.parseDouble(vInfo.get(e).lng));
-            double currDist = distance(currLoc,eLoc);
+        for (String v : vInfo.keySet()){
+            Pair<Double, Double> vLoc = new Pair<>(Double.parseDouble(vInfo.get(v).lat),Double.parseDouble(vInfo.get(v).lng));
+            double currDist = distance(currLoc,vLoc);
             if (currDist < minDist){
                 minDist = currDist;
-                minExhibit = e;
+                minVertex = v;
             }
         }
 
-        exhibits.remove(minExhibit);
-
-        List<GraphPath<String, IdentifiedWeightedEdge>> rest = rc.calculateRoute(minExhibit, exhibits);
-        route.addAll(rest);
-        return route;
+        return minVertex;
     }
-
 
     public static double distance (Pair<Double,Double> l1, Pair<Double,Double> l2){
         double base = 100;
